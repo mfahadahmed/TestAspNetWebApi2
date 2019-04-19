e47e7f56283132590ac0729bfe03659bac3dc58c
mfahadahmed <fahmed@folio3.com>
2019-04-19 18:40:56 +0500

    Added test application.

---
+++ ConsoleApp1/OptlyController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace ConsoleApp1
+{
+    public class OptlyController : ApiController
+    {
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "GetList", "value1", "value2" };
+        }
+
+        public string Get(int id)
+        {
+            return "Get" + id;
+        }
+
+        public string Post([FromBody]string value)
+        {
+            return "POST" + value;
+        }
+
+        public string Put(int id, [FromBody]string value)
+        {
+            return "PUT" + id + value;
+        }
+
+        public string Delete(int id)
+        {
+            return "DELETE" + id;
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Owin.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string baseAddress = "http://localhost:5000/";
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                HttpClient client = new HttpClient();
+                var response = client.GetAsync(baseAddress).Result;
+
+                Console.WriteLine(response);
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+                Console.ReadLine();
+            }
+        }
+    }
+}
